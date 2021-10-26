abfc0aa650cda8225969c56b61757d3dcf143965
Prem Sai <premsaiappasani@gmail.com>
2021-10-26 10:33:02 +0530

    changed array index in newBarcodebyDynamosodt

--- mispot/server.js
+++ mispot/server.js
@@ -219,7 +219,7 @@ app.get("/verify/barcode/:random",(req,res)=>{
     console.log(tid);
     console.log("abv");
     console.log(newBarcodeByDynamosoft);
-    let passingbarcodeNum=newBarcodeByDynamosoft[0];
+    let passingbarcodeNum=newBarcodeByDynamosoft[-1];
     res.render('barcode',{tid,passingbarcodeNum});
 });
 
